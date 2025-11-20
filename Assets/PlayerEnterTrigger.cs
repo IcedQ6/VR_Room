4263b60a26c67e83b7afcc838f6d4dac7c218caf
IcedQ6 <burningbabystudios@gmail.com>
2025-11-19 22:21:00 -0500

    Fixing collisions and animations

---
+++ Assets/PlayerEnterTrigger.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class PlayerEnterTrigger : MonoBehaviour
+{
+    
+}
